cd92f85944450931a524c6cbf3a76b5e78920142
Shane McIntosh <shanemcintosh@acm.org>
2021-03-04 17:40:44 -0500

    Initial commit

---
+++ src/main/java/ca/uwaterloo/cs446/smells/TrafficPatrol.java
@@ -0,0 +1,16 @@
+package ca.uwaterloo.cs446.smells;
+
+public class TrafficPatrol {
+	public void recordSpeedingViolation(String vehicleMake,
+			String vehicleModel, String vehicleColour,
+			String licensePlateNumber) {
+		// Implementation of business logic
+	}
+
+	public void recordParkingViolation
+			(String vehicleMake,
+			String vehicleModel, String vehicleColour,
+			String licensePlateNumber) {
+		// Implementation of business logic
+	}
+}
